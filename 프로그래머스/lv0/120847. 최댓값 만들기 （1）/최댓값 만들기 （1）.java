1f2ba64f9112d1f86ca79802357702ecacb3c759
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2023-04-13 03:12:36 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.34 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int idx = numbers.length;
+        return numbers[idx - 1] * numbers[idx - 2];
+    }
+}
