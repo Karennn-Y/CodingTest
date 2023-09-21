3663e87df928eeb9b9d1b9ffa680548bc98b4ce6
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2023-09-21 23:47:24 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.35 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        Arrays.sort(lottos);
+        Arrays.sort(win_nums);
+        int zero_cnt = 0, cnt = 0;
+
+        for (int lotto : lottos) {
+            if (lotto == 0) {
+                zero_cnt++;
+                continue;
+            }
+            for (int win_num : win_nums) {
+                if (win_num == lotto) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        return new int[]{7 - Math.max(cnt + zero_cnt, 1), 7 - Math.max(cnt, 1)};
+    }
+}
