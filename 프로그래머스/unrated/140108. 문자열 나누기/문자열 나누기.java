55091889e97e971638260a9ba71c27d1189c9f8e
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2023-09-25 21:34:41 +0900

    [unrated] Title: 문자열 나누기, Time: 10.50 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(String s) {
+        ArrayList<String> words = new ArrayList<>();
+        String[] arr = s.split("");
+        String x = arr[0];
+        int idx = 0;
+        int same = 1;
+        int different = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (same == different) {
+                //System.out.println(s.substring(idx, i));
+                words.add(s.substring(idx, i));
+                x = arr[i];
+                idx = i;
+                same = 1;
+                different = 0;
+            }
+            else if (x.equals(arr[i]))
+                same++;
+            else if (!x.equals(arr[i]))
+                different++;
+        }
+        
+        words.add(s.substring(idx, arr.length));
+        return words.size();
+    }
+}
