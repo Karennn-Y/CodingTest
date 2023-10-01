40e44d9c6efeb68514a52d3450b664bcfbb643d4
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2023-10-02 00:29:32 +0900

    [unrated] Title: 둘만의 암호, Time: 0.31 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,23 +1,6 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s, String skip, int index) {
-        ArrayList<Character> alphabet = new ArrayList<>();
-        for (int i = 0; i < 26; i++) {
-            // 아스키 코드 값에 해당하는 거 넣기
-            alphabet.add((char) ('a' + i));
-        }
-        
-        for (Character c : skip.toCharArray())
-            alphabet.remove(c);
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for (Character c : s.toCharArray()) {
-            int idx = alphabet.indexOf(c);
-            idx = (idx + index) % alphabet.size(); // 인덱스 값이 넘어갈 수 있으니깐..
-            sb.append(alphabet.get(idx));
-        }
-        return sb.toString();
+        String answer = "";
+        return answer;
     }
 }
