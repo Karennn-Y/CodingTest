bd31e37eb957c311b886077bd212dfd52bf47772
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2023-10-09 15:51:44 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 9.98 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,15 +1,11 @@
 class Solution {
     public String solution(int[] food) {
         StringBuilder sb = new StringBuilder();
-        StringBuilder sbRight = new StringBuilder();
         for (int i = 1; i < food.length; i++) {
             int div = food[i] / 2;
             sb.append(String.valueOf(i).repeat(Math.max(0, div)));
-            sbRight.append(String.valueOf(i).repeat(Math.max(0, div)));
         }
-        sbRight.reverse();
-        sb.append(0);
-        sb.append(sbRight);
-        return sb.toString();
+       
+        return sb.toString() + "0" + sb.reverse().toString();
     }
 }
