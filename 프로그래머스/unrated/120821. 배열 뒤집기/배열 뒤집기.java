d4ab8e3598d5c9a240c6021dd944670b8652f35b
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2023-10-11 01:56:29 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length];
-        int idx = num_list.length - 1;
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = num_list[idx--];
+        for(int i = 0; i< num_list.length; i++){
+            answer[i] = num_list[num_list.length-i-1];
         }
         return answer;
     }
