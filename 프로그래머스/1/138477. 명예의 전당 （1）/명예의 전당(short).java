bfdd181fd9b92c062232eab3f981c9e0c55f6ed5
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2023-10-12 12:01:31 +0900

    Create 명예의 전당(short).java

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당(short).java
@@ -0,0 +1,14 @@
+public static int[] solution (int[] score, int k) {
+        int[] answer = new int[score.length];
+        ArrayList<Integer> fame = new ArrayList<>();
+
+        for (int i = 0; i < score.length; i++) {
+            fame.add(score[i]);
+            Collections.sort(fame);
+            if (fame.size() > k)
+                fame.remove(0);
+            Collections.sort(fame);
+            answer[i] = fame.get(0);
+        }
+        return answer;
+    }
