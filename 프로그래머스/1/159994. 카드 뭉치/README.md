ca3a7fd23e176c3ad41e923bacd45abe3a53155c
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2023-10-15 04:30:31 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#) 
 
 ### 성능 요약
 
-메모리: 72.6 MB, 시간: 0.12 ms
+메모리: 83.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 14:8:27
+2023년 10월 0일 4:30:28
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,20 +2,17 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        List<String> c1 = new ArrayList<>(Arrays.asList(cards1));
-        List<String> c2 = new ArrayList<>(Arrays.asList(cards2));
-        int cnt = 0;
-        
-        for(String s : goal) {
-            if (!c1.isEmpty() && s.equals(c1.get(0))) {
-                c1.remove(0);
-                cnt++;
-            }
-            else if (!c2.isEmpty() && s.equals(c2.get(0))) {
-                c2.remove(0);
-                cnt++;
-            }
+        int idx1 = 0;
+        int idx2 = 0;
+
+        for (String g : goal) {
+            if (idx1 < cards1.length && g.equals(cards1[idx1]))
+                idx1++;
+            else if (idx2 < cards2.length && g.equals(cards2[idx2]))
+                idx2++;
+            else
+                return "No";
         }
-        return cnt == goal.length ? "Yes" : "No";
+        return "Yes";
     }
 }
