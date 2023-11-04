63b50305cc704140efc7c82556c5dc36b3fe3cee
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2023-11-05 01:24:03 +0900

    [level 2] Title: 올바른 괄호, Time: 21.26 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,29 +2,15 @@ import java.util.Stack;
 
 class Solution {
     boolean solution(String s) {
-//         Stack<String> box = new Stack<>();
-        
-//         if (s.startsWith(")"))
-//             return false;
-        
-//         for (String str : s.split("")) {
-//            if (str.equals(")") && box.peek().equals("(") && !box.isEmpty())
-//                 box.pop();
-//             else
-//                 box.push(str);
-//         }
-//         return box.isEmpty();
-        boolean answer = true;
         Stack<Character> stack = new Stack<>();
-        if (s.charAt(0) == ')') answer = false;
+        
+        if (s.charAt(0) == ')') return false;
 
         for (int i = 0; i < s.length(); i++){
             if (!stack.isEmpty() && s.charAt(i) == ')') stack.pop();
             else stack.push(s.charAt(i));
         }
-
-
-        if (!stack.isEmpty()) return answer = false;
-        else return answer;
+        
+        return stack.isEmpty();
     }
 }
