b31004be26ce49fa1e1c61e21517dbf264feb661
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2024-05-16 21:09:04 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        HashMap<String, Integer> wishList = new HashMap<>();
+
+        for (int i = 0; i < want.length; i++) {
+            wishList.put(want[i], number[i]);
+        }
+
+        for (int i = 0; i <= discount.length - 10; i++) {
+            HashMap<String, Integer> saleList = new HashMap<>();
+            for (int j = 0; j < 10; j++) {
+                saleList.put(discount[i + j], 
+                             saleList.getOrDefault(discount[i + j], 0) + 1);
+            }
+            
+            boolean flag = true;
+            
+            for (String key : wishList.keySet()) {
+                if (wishList.get(key) != saleList.get(key)) {
+                    flag = false;
+                    break;
+                }
+            }
+            answer += (flag) ? 1 : 0;
+        }
+        return answer;
+    }
+}
